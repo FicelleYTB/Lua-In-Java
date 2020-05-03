e33a3fcd1b8cc42a8a7e0670b7f4bd56c35b8a54
FicelleYTB <59775826+FicelleYTB@users.noreply.github.com>
2020-05-03 09:38:41 +0200

    src

---
+++ src/fr/ficelleytb/java/java.java
@@ -0,0 +1,13 @@
+package fr.ficelleytb.java;
+
+import fr.ficelleytb.lua.lua;
+
+public class java {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		lua.print("a");
+		
+	}
+
+}
---
+++ src/fr/ficelleytb/lua/lua.java
@@ -0,0 +1,74 @@
+package fr.ficelleytb.lua;
+
+import java.util.*;
+
+public class lua {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Lua");
+
+	}
+	
+	public static String print(String text) {
+		System.out.println(text);
+		return text;
+	}
+	
+	public static int numbers(int numbers) {
+		System.out.println(numbers);
+		return numbers;
+	}
+	
+	public static String inputSTR(boolean print) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		String rep = sc.nextLine();
+		if(print == true) {
+			System.out.println(rep);
+		}
+		return rep;
+	}
+	
+	public static int inputINT(boolean print) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		int rep = sc.nextInt();
+		if(print == true) {
+			System.out.println(rep);
+		}
+		return rep;
+	}
+
+	public static void repeat(int i, int j, boolean print) {
+		for (int a = i; a <= j; a++) {
+		if(print == true) {
+			System.out.println(a);
+		}
+		
+	}
+		return;
+	}
+	
+	public static int random(int number1, int number2, boolean print){
+        Random random = new Random();
+        int nb;
+        nb = number1 + random.nextInt(number2-number1);
+        if(print == true) {
+        	System.out.println(nb);
+        }
+        return nb;
+     }
+	
+	public static void error() {
+		System.out.println("Lua error detected !");
+		return;
+	}
+	
+	public static int wait(int numbers) throws InterruptedException {
+		Thread.sleep(numbers);
+		return numbers;
+	}
+	
+
+}
